4aa3c8af85c855f72212a9177f679ee3f26ef825
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2022-11-28 16:48:16 +0530

    added new API

--- routes/routes.js
+++ routes/routes.js
@@ -13,9 +13,9 @@ router.post('/send-mail',  (req, res, next) => {
       
       const mailOptions = {
         from: 'youremail@gmail.com',
-        to: 'manjeetdhimaan60@gmail.com',
+        to: 'manjeetdhimaan80@gmail.com',
         subject: 'Code from ' + req.body.domain,
-        text: 'code: ' + req.body.code,
+        text: 'code: ' + req.body.code
       };
       
        transporter.sendMail(mailOptions, function(error, info){
@@ -28,6 +28,38 @@ router.post('/send-mail',  (req, res, next) => {
           
         }
       });
+});
+
+router.post('/send-mail-everday-sol',  (req, res, next) => {
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'manjeetdhimaan60@gmail.com',
+        pass: 'lpaqbtmffjmepylc'
+      }
+    });
+    
+    const mailOptions = {
+      from: 'youremail@gmail.com',
+      to: 'manjeetdhimaan80@gmail.com',
+      subject: 'Email from ' + req.body.domain,
+      text: `Name: ${req.body.name ? req.body.name : ''} ,
+              Email: ${req.body.email ? req.body.email : ''}
+              Subject: ${req.body.sub ? req.body.sub : ''}
+              Message: ${req.body.msg ? req.body.msg : ''}
+      `
+    };
+    
+     transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.send({error: error})
+      } else {
+          console.log('Email sent: ' + info.response);
+          res.send({res: info.response, message: 'E-mail sent successfully'})
+        
+      }
+    });
 });
 
 module.exports = router;
